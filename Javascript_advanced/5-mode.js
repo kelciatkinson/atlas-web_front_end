170a5dfb05cca693b763f4995019b9350804b524
“kelciatkinson” <kelciatkinson@gmail.com>
2024-11-19 15:49:21 -0600

    task 6

---
+++ Javascript_advanced/5-mode.js
@@ -0,0 +1,37 @@
+function changeMode(size, weight, transform, background, color) {
+  return function() {
+    document.body.style.fontSize = size;
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  }
+}
+
+function main() {
+  let spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  let darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+  let screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+
+  let paragraph =  document.createElement('p');
+  paragraph.textContent = 'Welcome Atlas!';
+  document.body.appendChild(paragraph);
+
+  let spookyButton = document.createElement('button');
+  spookyButton.textContent = 'Spooky';
+  spookyButton.addEventListener('click', spooky);
+
+  let darkModeButton = document.createElement('button');
+  darkModeButton.textContent = 'Dark mode';
+  darkModeButton.addEventListener('click', darkMode);
+
+  let screamModeButton = document.createElement('button');
+  screamModeButton.textContent = 'Scream mode';
+  screamModeButton.addEventListener('click', screamMode);
+
+  document.body.appendChild(spookyButton);
+  document.body.appendChild(darkModeButton);
+  document.body.appendChild(screamModeButton);
+}
+
+main()
